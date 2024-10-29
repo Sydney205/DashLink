c59a7765428ac4dd80fca52efe83bf32a2ac21f5
Sydney205 <diekechi@gmail.com>
2024-10-30 00:55:39 +0100

    hosting prep

--- App/__init__.py
+++ App/__init__.py
@@ -4,10 +4,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-# from dotenv import load_dotenv
-
-# Load environment variables from .env file
-# load_dotenv()
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -15,10 +11,6 @@ config.read('config.ini')
 app = Flask(__name__)
 app.config['SECRET_KEY'] = config.get('APP', 'SECRET_TOKEN')
 app.config['SQLALCHEMY_DATABASE_URI'] = config.get('APP', 'DATABASE_URL')
-
-# app = Flask(__name__)
-# app.config['SECRET_KEY'] = os.getenv("SECRET_TOKEN")
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
